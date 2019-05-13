280a63b5648b3fe97dbf6ded37c9dee6e996b9f8
Marko Arandjelovic <marko977x@gmail.com>
2019-05-14 01:16:45 +0200

    Add domain models

---
+++ PHOTOnline/PHOTOnline.Domain/Entities/Enums/ProductEnums.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.Entities.Enums
+{
+    public enum ProductType
+    {
+        PhotoBook = 1, Calendar = 2, Mug = 3, Photo = 4
+    }
+
+    public enum CoverType
+    {
+
+    }
+
+    public enum CalendarType
+    {
+
+    }
+
+    public enum MugType
+    {
+
+    }
+}
