37c582755649a8957a57b10462357a06db32eb8c
Marko Arandjelovic <marko977x@gmail.com>
2019-05-22 13:58:42 +0200

    Add blob storage

---
+++ PHOTOnline/PHOTOnline.BlobStorage/BlobStoreOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PHOTOnline.BlobStorage
+{
+    public class BlobStoreOptions
+    {
+        public string ConnectionString { get; set; }
+        public string ContainerName { get; set; }
+    }
+}
---
+++ PHOTOnline/PHOTOnline.BlobStorage/ServiceCollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PHOTOnline.BlobStorage
+{
+    public static class ServiceCollectionExtension
+    {
+        private const string BLOB_STORAGE_SECTION = "BlobStorage";
+        public static void AddBlobStore(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IBlobStore, BlobStore>();
+            services.Configure<BlobStoreOptions>(configuration.GetSection(BLOB_STORAGE_SECTION));
+        }
+    }
+}
