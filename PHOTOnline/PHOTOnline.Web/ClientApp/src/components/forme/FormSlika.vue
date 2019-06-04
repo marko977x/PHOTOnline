c9eea70605cd899fccd4fb9bb510f51e26e43fc5
Marko Arandjelovic <marko977x@gmail.com>
2019-06-05 00:32:01 +0200

    Fix FormSlika

--- PHOTOnline/PHOTOnline.Web/ClientApp/src/components/forme/FormSlika.vue
+++ PHOTOnline/PHOTOnline.Web/ClientApp/src/components/forme/FormSlika.vue
@@ -2,32 +2,21 @@
     <el-dialog visible center
         @close="$emit('zatvoriSliku')"
         :modalAppendToBody="false">
-        <div class="photoContainer"><img :src="photo.Thumbnail.Url"/></div>
+        <div class="photoContainer"><img :src="shownPhoto.Thumbnail.Url"/></div>
     </el-dialog>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            photo: this.shownPhoto
-        }
-    },
     props: ['shownPhoto']
 }
 </script>
 
 <style scoped>
-
-.el-dialog{
-    height: 100%;
-}
-
 .photoContainer {
     display: flex;
     justify-content: center;
 }
-
 </style>
 
 
