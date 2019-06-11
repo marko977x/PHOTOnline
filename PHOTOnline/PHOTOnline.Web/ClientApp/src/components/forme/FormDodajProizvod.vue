ce9ac455d66dbedd13dd5ab284c07e6fb0442689
Marko Arandjelovic <marko977x@gmail.com>
2019-06-11 16:43:30 +0200

    Dodavanje proizvoda

--- PHOTOnline/PHOTOnline.Web/ClientApp/src/components/forme/FormDodajProizvod.vue
+++ PHOTOnline/PHOTOnline.Web/ClientApp/src/components/forme/FormDodajProizvod.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="dodaj-proizvod-container">
-        <el-dialog visible width="28%" heigth="30%" title="Dodavanje proizvoda" @close="$emit('zatvoriDodavanjeProizvoda')" :modal-append-to-body="false">
-            <el-form>
+        <el-dialog visible width="28%" 
+            heigth="30%" title="Dodavanje proizvoda" 
+            @close="$emit('zatvoriDodavanjeProizvoda')" 
+            :modal-append-to-body="false">
+            <el-form v-loading="isSpinnerActive">
                 <div class="stavka">
                     <label>Naziv:</label>
                     <el-input v-model="proizvod.Title" placeholder="Naziv proizvoda" size="small"></el-input>
@@ -23,7 +26,7 @@
                     <input type="file" accept="image/*" @change="uploadImages($event)" id="file-input" >
                 </div>
                 <div class="stavka-2">
-                    <el-button  type="primary" size="mini" @click="validacija()">Potvrdi</el-button>
+                    <el-button  type="primary" size="mini" @click="addProduct()">Potvrdi</el-button>
                 </div>
             </el-form>
         </el-dialog>
@@ -36,26 +39,46 @@
     export default {
         data() {
             return {
-                proizvod: { Title: '', Price: '', Size: 'Univerzalni', Description: '', ProductType: '', ImageUrl: '' },
+                proizvod: { 
+                    Title: '', 
+                    Price: '',
+                    Size: 'Univerzalni',
+                    Description: '',
+                    ProductType: '',
+                    ImageUrl: '' 
+                },
+                isSpinnerActive: false,
+                isUploadingDone: false,
+                uploadingImagesInProgress: false
             }
         },
         methods: {
             validacija(){
-                if(this.proizvod.Title != '' && this.proizvod.Price != '' && this.proizvod.ProductType != '' && this.proizvod.ImageUrl != '')
-                     this.prihvatiUnosForme();
-                else
+                if(this.proizvod.Title == '' || this.proizvod.Price == '' || this.proizvod.ProductType == '' || this.proizvod.Description == '') {
                     this.$message({message : "Morate uneti polja za naziv, cenu i tip proizvoda, kao i upload-ovati sliku.", type: "warning"});
+                    return false;
+                }
+                else if(this.proizvod.ImageUrl == '' && !this.uploadingImagesInProgress) {
+                    this.$message({message: "Morate upload-ovati bar jednu sliku", type: "warning"});
+                    return false;
+                }
+                else return true;
             },
-            prihvatiUnosForme() {
-                apiFetch('POST', destinationUrl + "/Product/AddShopProduct", this.proizvod)
-                    .then(result => {
-                        this.$message("Uspešno ste dodali novi proizvod!");
-                        this.$emit("zatvoriDodavanjeProizvoda");
-                    }).catch(error => {
-                        console.log(error);
-                    });
+            async addProduct() {
+                if(!this.validacija()) return;
+                if(this.isUploadingDone && !this.isSpinnerActive) {
+                    apiFetch('POST', destinationUrl + "/Product/AddShopProduct", this.proizvod)
+                        .then(result => {
+                            this.$message("Uspešno ste dodali novi proizvod!");
+                            this.$emit("zatvoriDodavanjeProizvoda");
+                        }).catch(error => {
+                            console.log(error);
+                        });
+                }
+                else this.isSpinnerActive = true;
             },
             uploadImages(event) {
+                this.uploadingImagesInProgress = true;
                 const formData = new FormData();
                 formData.append("image", event.target.files[0]);
                 fetch(destinationUrl + "/Image/UploadImage", {method: 'POST', body: formData})
@@ -64,6 +87,12 @@
                     }).then(result => {
                         console.log(result.Data);
                         this.proizvod.ImageUrl = result.Data.Image.Small.Url;
+                        this.uploadingImagesInProgress = false;
+                        this.isUploadingDone = true;
+                        if(this.isSpinnerActive) {
+                            this.isSpinnerActive = false;  
+                            this.addProduct();
+                        }
                     }).catch(error => {console.log(error)});
             }
         }
